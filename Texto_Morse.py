de0e4e56a3924396e03518aae5b2a8b437466faa
FlintDLoco <109046149+FlintDLoco@users.noreply.github.com>
2022-07-24 07:02:54 -0400

    Add files via upload

---
+++ Texto_Morse.py
@@ -0,0 +1,18 @@
+from dictionary import DiccionarioMorse
+morse_text = []
+
+def main():
+    start = "Traductor texto a morse\n"
+    print("*" * len(start))
+    print(start + "*" * len(start) + "\n")
+    morse = input("Ingresa tu texto a morse: ")
+    for a in morse:
+        if a in DiccionarioMorse:
+            print(DiccionarioMorse[a], end=" ")
+            morse_text.append("{} / {}".format(a, DiccionarioMorse[a]))
+    print("\n")
+    print("Texto a traducir: {}. Traduccion: {}\n".format(morse, morse_text))
+
+
+if __name__ == "__main__":
+    main()
